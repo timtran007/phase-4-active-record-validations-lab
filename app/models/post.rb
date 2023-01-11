b8577e55638363a635b3e7a93dad0de5d78fcde9
Tim <timtran007@gmail.com>
2023-01-11 00:40:28 -0600

    commit tree 68eaaae64b1b970cd8414d82931038b3475ab33e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :no_true_facts_title
+
+    def no_true_facts_title
+        if title == "True Facts"
+            errors.add(:title, "True Facts as a title is clickbait")
+        end
+    end
 end
